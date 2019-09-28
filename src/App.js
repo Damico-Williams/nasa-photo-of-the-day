0bdb018ca81418b7d98a81ce1992a368ca5aa32f
damico williams <damicowilliams@gmail.com>
2019-09-28 15:59:53 -0700

    nasa MVP

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
-import nasaComponent from "./index.js";
+// import DatePicker from "./NasaCom/button.js"
+
+import axios from "axios";
+import { setDate } from "date-fns";
 
 function App() {
+  
+    const [image, setImage] = useState([])
+    const [paragraphState, setparagraphState] = useState([]);
+    const [date, setDate] = useState([])
+   
+    
+    useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        .then(response => {
+            console.log(response.data.url);
+            setImage(response.data.url);
+            setparagraphState(response.data.explanation);
+            setDate(response.data.date)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+
+
   return (
     <div className="App">
-      <h1>Nasa Photos</h1>
+      <h1>Nasa Photo of the Day</h1>
+      <h3>{date}</h3>
       {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p> */}
-      <nasaComponent />
+      <img src={image} />
+      <p>{paragraphState}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
 
-
-export function nasaComponent() {
-    const [image, setImage] = useState([])
-
-    useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-        .then(response => {
-            console.log(response.data.url);
-            setImage(response.data.url);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }, [])
-}
 
 
 
