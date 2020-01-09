faf4a3b9422dcbb79cff0e55b63a5c203b52bc9c
damico williams <damicowilliams@gmail.com>
2020-01-08 18:47:25 -0800

    update added a file data.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,15 @@
-import React, {useEffect, useState} from "react";
-import styled from 'styled-components';
+import React from "react";
 import "./App.css";
-import axios from "axios";
-
-const Button = styled.button`
-   padding:0.6rem 1 rem;
-   margin: 0.5rem;
-   border:none;
-   border-radius: 0.3rem;
-   background-color:#99ccff;
-`;
-
-const Text = styled.h3`
-  font-size: 3.5rem;
-`;
+import Data from "./Data.js"
 
 
 function App() {
-  
-    const [image, setImage] = useState([])
-    const [paragraphState, setparagraphState] = useState([]);
-    const [date, setDate] = useState([])
-   
-    
-    useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-        .then(response => {
-            console.log(response.data.url);
-            setImage(response.data.url);
-            setparagraphState(response.data.explanation);
-            setDate(response.data.date)
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }, [])
 
 
   return (
     <div className="App">
       <h1>Nasa Photo of the Day</h1>
-      <Button>random</Button>
-      <Text>{date}</Text>
-      {/* <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p> */}
-      <img src={image} />
-      <p>{paragraphState}</p>
+     <Data/>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from "react";
+import styled from 'styled-components';
+import axios from "axios";
+
+
+const Button = styled.button`
+   padding:0.6rem 1 rem;
+   margin: 0.5rem;
+   border:none;
+   border-radius: 0.3rem;
+   background-color:#99ccff;
+`;
+
+const Text = styled.h3`
+  font-size: 3.5rem;
+`;
+
+
+const Data = () => {
+
+    const [image, setImage] = useState([])
+    const [paragraphState, setparagraphState] = useState([]);
+    const [date, setDate] = useState([])
+   
+    
+    useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        .then(response => {
+            console.log(response.data.url);
+            setImage(response.data.url);
+            setparagraphState(response.data.explanation);
+            setDate(response.data.date)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+
+    return (
+    <div className="App">
+    <Button>random</Button>
+    <Text>{date}</Text>
+    {/* <p>
+      Read through the instructions in the README.md file to build your NASA
+      app! Have fun 🚀!
+    </p> */}
+    <img src={image}/>
+    <p>{paragraphState}</p>
+  </div>
+  )
+    
+}
+
+export default Data;
